cb93ae099ab43bf528d91a3d13682d61a44206e1
rodrigo <rodrigocsilva63@gmail.com>
2022-09-14 21:40:05 -0300

    add dinamic games with API connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './styles/main.css'
 import logo from './assets/Logo.svg'
-import {MagnifyingGlassPlus} from 'phosphor-react';
-import GameCard from './components/GameCard';
+import { GameCard } from './components/GameCard';
+import { CreateAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+
+interface Games {
+  id: string,
+  title: string,
+  bannerUrl: string,
+  _count: {
+    ads: number
+  }
+}
 
 function App() {
+
+  const [games, setGames] = useState<Games[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+
+  }, [])
+
+
   return (
+
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logo} />
       <h1 className='text-6xl text-white font-black mt-20'>
@@ -12,48 +36,20 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-
-        <GameCard
-          title='League of legends'
-          ads='4 anúncios'
-        />
-        <GameCard
-          title='League of legends'
-          ads='4 anúncios'
-        />
-        <GameCard
-          title='League of legends'
-          ads='4 anúncios'
-        />
-        <GameCard
-          title='League of legends'
-          ads='4 anúncios'
-        />
-        <GameCard
-          title='League of legends'
-          ads='4 anúncios'
-        />
-        <GameCard
-          title='League of legends'
-          ads='4 anúncios'
-        />
-        
+        {games.map(game => {
+          return (
+            <GameCard
+              key={game.id}
+              title={game.title}
+              ads={game._count.ads}
+              bannerUrl={game.bannerUrl}
+            />
+          )
+        })
+        }
       </div>
 
-      <div className='bg-gradient pt-1 self-stretch rounded-lg overflow-hidden mt-8'>
-        <div className='bg-[#2A2634] w-full py-6 px-8 flex justify-between items-center'>
-          <div>
-            <strong className='text-2xl text-white' >Não encontrou seu duo?</strong>
-            <span className='block text-zinc-400'>Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className='py-3 px-4 text-white bg-violet-500 hover:bg-violet-600 rounded flex gap-3'>
-            <MagnifyingGlassPlus size={24}/>
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
-
+        <CreateAdBanner />
     </div>
   )
 }
