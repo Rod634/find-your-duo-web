06e7e15cbcd93a694a75eb4b7bcf3b04a872039e
rodrigo <rodrigocsilva63@gmail.com>
2022-09-14 21:40:27 -0300

    gameCard component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,16 +1,17 @@
 
 interface gameCardProps {
     title: string,
-    ads: string
+    ads: number,
+    bannerUrl: string
 }
 
-export default function GameCard({title, ads} : gameCardProps){
+export function GameCard({title, ads, bannerUrl} : gameCardProps){
     return (
         <a className='relative rounded-lg overflow-hidden'>
-          <img src="game-img.png"/>
+          <img src={bannerUrl}/>
           <div className='w-full pt-16 pb-4 px-4 bg-game-card-gradient absolute bottom-0 left-0 right-0'>
             <strong className='text-white'>{title}</strong>
-            <span className='text-zinc-300 text-sm block'>{ads}</span>
+            <span className='text-zinc-300 text-sm block'>{ads}{ads > 1 ? ' anúncios' : ' anúncio'} </span>
           </div>
         </a>
     )
